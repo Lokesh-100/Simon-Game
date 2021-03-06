6c00dc81e49ce1326f428f503662bcf599bb6925
Lokesh-100 <lokeshboddupalli11699@gmail.com>
2021-03-06 12:01:17 +0530

    Add files via upload

---
+++ Simon Game/game.js
@@ -0,0 +1,78 @@
+var buttonColors=["red","blue","green","yellow"];
+var gamePattern=[];
+var userClickedPattern=[];
+
+
+var level=0;
+var started=false;
+$(document).keypress(function() {
+  if (!started) {
+    nextSequence();
+    started = true;
+  }
+});
+
+function nextSequence(){
+
+  userClickedPattern=[];
+  level=level+1;
+    $("#level-title").text("Level " + level);
+  var randomNumber = Math.floor(Math.random()*4);
+  randomChosenColour = buttonColors[randomNumber];
+  gamePattern.push(randomChosenColour);
+  $("#"+randomChosenColour).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+
+}
+$(".btn").click(function() {
+
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
+  animatePress(userChosenColour);
+
+  checkAnswer(userClickedPattern.length-1);
+});
+
+function playSound(name){
+  var audio = new Audio("sounds/"+name+".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor){
+  $("#"+currentColor).addClass("pressed");
+  setTimeout(function(){
+            $('#'+currentColor).removeClass("pressed");
+        },100);
+}
+
+function checkAnswer(currentLevel)
+{
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    if (userClickedPattern.length === gamePattern.length){
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  }
+    else
+    {
+    playSound("wrong");
+          $("body").addClass("game-over");
+            $("#level-title").text("Game Over, Press Any Key to Restart");
+      setTimeout(function(){
+                $("body").removeClass("game-over");
+            },200);
+
+
+      console.log("failure");
+      startOver();
+    }
+    }
+
+  function startOver(){
+    level=0;
+    gamePattern=[];
+    started=false;
+  }
